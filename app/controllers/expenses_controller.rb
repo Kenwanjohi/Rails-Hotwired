44c2f2abb97e7e4490c6054f2e7ec2459088e388
Kenwanjohi <wanjohiken85@gmail.com>
2023-04-05 00:26:42 +0300

    Create new expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,23 @@
 class ExpensesController < ApplicationController
   def index
     @expenses = Expense.all
+    @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.new(expense_params)
+    respond_to do |format|
+      if @expense.save
+        format.html { redirect_to expenses_url, notice: "Expense was successfully created" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:description, :amount, :date)
   end
 end
